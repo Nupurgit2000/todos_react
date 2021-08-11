8ad4393d06c566fe2e4a2b376ca8d29e34f1ced9
Nupur Goel <goelnupur123@gmail.com>
2021-08-11 21:08:30 +0530

    Initial Commit

---
+++ todos-list/src/MyComponents/About.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            This is an about component
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate exercitationem harum culpa, ipsam molestiae repudiandae quod aliquid reprehenderit et dolore at dolorem ratione. Nostrum suscipit temporibus asperiores placeat recusandae rerum obcaecati consectetur nulla facere!
+            </p>
+        </div>
+    )
+}
